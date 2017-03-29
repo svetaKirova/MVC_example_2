310371f4eea47eb167821113387b5efa5a01683c
svetaKirova <svetaKirova@yahoo.com>
2017-03-29 13:45:00 +0300

    mvcLecture 1-7

---
+++ controllers/pages.controller.php
@@ -0,0 +1,19 @@
+<?php
+
+class PagesController extends Controller
+{
+    public function index()
+    {
+        $this->data['test_content'] = 'Here will be a pages list';
+
+    }
+
+    public function view()
+    {
+        $params = App::getRouter()->getParams();
+        if (isset($params[0])) {
+            $alias = strtolower(($params[0]));
+            $this->data['content'] = "Here will be page with '{$alias}' alias";
+        }
+    }
+}
---
+++ lib/app.class.php
@@ -0,0 +1,41 @@
+<?php
+
+class App
+{
+    protected static $router;
+
+    /**
+     * @return mixed
+     */
+    public static function getRouter()
+    {
+        return self::$router;
+    }
+    //Method which is responsible for processing(обработка) of requests of application
+    public static function run($uri){
+        self::$router = new Router($uri);
+
+        $controller_class = ucfirst(self::$router->getController()).'Controller';
+        $controller_method = strtolower(self::$router->getMethodPrefix().self::$router->getAction());
+
+        //Collong controller's method
+        $controller_object = new $controller_class();
+
+        if(method_exists($controller_object, $controller_method )){
+            //Controller's action may return a view
+            $view_path = $controller_object->$controller_method();
+            $view_object = new View($controller_object->getData(), $view_path);
+            $content = $view_object->render();
+        } else{
+            // не срабатывает исключение
+            //throw new Exception('nononono');
+            echo "Method '$controller_method' of '$controller_class' doesn't exist";
+
+        }
+        $layout = self::$router->getRoute();
+        $layout_path = VIEWS_PATH.DS.$layout.'.html';
+        $layout_view_object = new View(compact('content'), $layout_path);
+        echo $layout_view_object->render();
+
+    }
+}
