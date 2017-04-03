34e632d72d25f416a160e6fb068929a38141d886
svetaKirova <svetaKirova@yahoo.com>
2017-04-03 18:09:30 +0300

    Lecture 14 Admin section part 2 of 2

--- controllers/contacts.controller.php
+++ controllers/contacts.controller.php
@@ -2,17 +2,25 @@
 
 class ContactsController extends Controller
 {
-    public function __construct ($data = array()) {
+    public function __construct($data = array())
+    {
         parent::__construct($data);
         $this->model = new Message();
     }
+
     public function index()
     {
-        if($_POST){
-           if($this->model->save($_POST) ){
-             Session::setFlash('Спасибо. Ваше сообщение отправлено.');
-           }
+        if ($_POST) {
+            if ($this->model->save($_POST)) {
+                Session::setFlash('Спасибо. Ваше сообщение отправлено.');
+            }
         }
+    }
+
+
+    public function admin_index()
+    {
+        $this->data = $this->model->getList();
     }
 
 }
